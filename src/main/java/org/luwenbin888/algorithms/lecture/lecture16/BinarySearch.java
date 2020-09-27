8a3b62b14a38ae7b855d992aa3f64c491b09382d
Lu Wenbin <lwb888leetcode@gmail.com>
2020-09-27 20:03:38 +0800

    add lecture 16 p2

--- src/main/java/org/luwenbin888/algorithms/lecture/lecture16/BinarySearch.java
+++ src/main/java/org/luwenbin888/algorithms/lecture/lecture16/BinarySearch.java
@@ -33,11 +33,49 @@ public class BinarySearch {
         return -1;
     }
 
+    public static int firstBiggerThanOrEqual(int[] arr, int data) {
+        if (arr == null || arr.length == 0) return -1;
+
+        int low = 0, high = arr.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (arr[mid] < data) low = mid + 1;
+            else {
+                if (mid == 0 || arr[mid - 1] < data) return mid;
+                else high = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+
+    public static int lastSmallerThanOrEqual(int[] arr, int data) {
+        if (arr == null || arr.length == 0) return -1;
+        int low = 0, high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] > data) high = mid - 1;
+            else {
+                if (mid == arr.length - 1 || arr[mid + 1] > data) return mid;
+                low = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+
     public static void main(String[] args) {
         int[] arr = {1,3,4,5,6,8,8,8,11,18};
         BinarySearch sol = new BinarySearch();
         System.out.println(sol.firstMatch(arr, 8));
 
         System.out.println(sol.lastMatch(arr, 8));
+
+        int[] arr2 = {3,4,6,8,9,10};
+        System.out.println(sol.firstBiggerThanOrEqual(arr2, 7));
+
+        System.out.println(sol.lastSmallerThanOrEqual(arr, 8));
     }
 }
